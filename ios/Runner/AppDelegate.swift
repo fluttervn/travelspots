21c989515b3b2a86c866482620495509548ea5d2
An Lam <anlam12787@gmail.com>
2019-11-27 11:02:31 +0700

    Add location permision

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyD7hIg6eke9JpnvKOC2T06aZ3SEh9OXRRY")
+    GMSServices.provideAPIKey("your_key")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
