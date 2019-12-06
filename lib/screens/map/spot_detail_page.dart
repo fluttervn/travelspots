ad3a0fde28ddf1751d5d6089e8cab789000bca78
An Lam <anlam12787@gmail.com>
2019-12-06 13:49:32 +0700

    layout detail page

--- lib/screens/map/spot_detail_page.dart
+++ lib/screens/map/spot_detail_page.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:travelspots/repos/models/data_models/app_database_entity.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SpotDetailPage extends StatelessWidget {
   final SpotEntity spotDataModel;
 
   SpotDetailPage({this.spotDataModel});
+
+  _launchWikiURL(String url) async {
+    const url = 'https://goo.gl/maps/GtRgtRqrALiD2jJ49';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Future<void> _launchGoogleURL(String url) async {
+    const url = 'https://goo.gl/maps/GtRgtRqrALiD2jJ49';
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: false,
+        forceWebView: false,
+        headers: <String, String>{'my_header_key': 'my_header_value'},
+      );
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +38,41 @@ class SpotDetailPage extends StatelessWidget {
       children: <Widget>[
         ListTile(
           title: Text(spotDataModel.name),
-          subtitle: Text(spotDataModel.address),
+          subtitle: Text('${spotDataModel.address}, ${spotDataModel.district}'),
+        ),
+        SizedBox(
+          height: 8.0,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                  side: BorderSide(color: Colors.blue, width: 1)),
+              color: Colors.blue,
+              child: Text(
+                'View on Google',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                _launchGoogleURL(spotDataModel.locationLink);
+              },
+            ),
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                  side: BorderSide(color: Colors.blue, width: 1)),
+              color: Colors.white,
+              child: Text(
+                'View on Wiki',
+                style: TextStyle(color: Colors.blue),
+              ),
+              onPressed: () {
+                _launchWikiURL(spotDataModel.locationLink);
+              },
+            )
+          ],
         )
       ],
     );
