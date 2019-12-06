ee9843399ce14f908022dbd1c3cecd072459895c
Triet Bui <x3cafe@gmail.com>
2019-12-06 11:41:57 +0700

    Temporary layout for CheckForUpdate

--- lib/screens/launcher/update_page.dart
+++ lib/screens/launcher/update_page.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class CheckForUpdatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('In CheckForUpdate page, you must wait for update is '
-            'done before continuing to HomePage or MapPage'),
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SpinKitRing(
+            color: Colors.blue,
+            size: 100,
+            duration: Duration(milliseconds: 700),
+          ),
+          SizedBox(height: 60),
+          Text(
+            'Updating app data. Please wait...',
+            style: TextStyle(fontWeight: FontWeight.normal, fontSize: 18),
+          ),
+        ],
       ),
     );
   }
